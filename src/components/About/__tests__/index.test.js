76a94b0dd0012ca5b610407ad1b7eedd745d4019
Zurisha <andreamwest7@gmail.com>
2022-07-24 11:50:06 -0400

    tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import{ render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    //first Test
+    it('renders', () => {
+        render(<About />);
+    });
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render.apply(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
